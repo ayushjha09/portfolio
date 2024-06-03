ce170aef072602d17e4a366948a2926432e08c22
Ayush jha <163509089+ayushjha09@users.noreply.github.com>
2024-06-04 01:22:49 +0530

    add javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ["Web Developer","Youtuber","web Designer","Influencer"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true
+  });
